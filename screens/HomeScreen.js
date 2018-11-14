d55d5b534aee511f6424b86d0fd65ec799ee0d8f
Damion Hardy <damionhardy@Damos-MacBook-Pro-8.local>
2018-11-14 17:13:38 +0000

    homescreen backgroup image loaded and working on expo

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { StyleSheet, Text, View, Button, Image } from 'react-native';
 import { createStackNavigator, StackActions, NavigationActions } from 'react-navigation';
+import { ImageBackground } from 'react-native';
 
 
 
@@ -8,7 +9,11 @@ class HomeScreen extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Home Screen</Text>
+      <ImageBackground
+          source={require('../assets/images/homeBackground.jpg')}
+          style={styles.homeBackground}>
+          <Text>Home Screen</Text>
+      </ImageBackground>
         <Button
         title="Scan"
         onPress={ () => {
@@ -28,10 +33,17 @@ class HomeScreen extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    // backgroundColor: '#fff',
+    // background: 'url(./assets/images/homeBackground.jpg)'
     alignItems: 'center',
     justifyContent: 'center',
   },
+  homeBackground: {
+    flex: 1,
+    width: '100%',
+    height: '100%',
+    resizeMode: 'cover'
+  }
 });
 
 export default HomeScreen;
