a3043d56159352a6a7544f0491d46248ce6d2f1e
AlexDresco <alexandra.d1510@gmail.com>
2018-11-14 15:27:28 +0000

    Data for Rory

--- components/Results.js
+++ components/Results.js
@@ -1,1 +1,25 @@
-// kgg
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class Results extends React.Component {
+  getData() {
+  return fetch("https://world.openfoodfacts.org/api/v0/product/737628064502.json")
+    .then(response => {
+      console.log(response);
+      return response.json();
+    })
+    .then(responseJson => {
+      this.setState({ data: responseJson });
+    })
+  }
+
+  render(){
+    return (
+      <View>
+      <Text>
+        {this.state.data}
+      </Text>
+      </View>
+    )
+  }
+}
