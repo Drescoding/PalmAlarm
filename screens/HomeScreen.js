826129cb9b4ad50ae1303975e852004c85f772c2
Amy Denny-Guerin <amydenny-guerin@Amys-MacBook-Pro.local>
2018-11-16 17:04:49 +0000

    Custom fonts working

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,38 +1,48 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button, Image, ImageBackground, Platform} from 'react-native';
+import { StyleSheet, Text, View, Image, Button, ImageBackground, Platform} from 'react-native';
 import { createStackNavigator, StackActions, NavigationActions } from 'react-navigation';
-import { font } from 'Expo';
+import { AppLoading, Font } from 'expo';
 
 class HomeScreen extends React.Component {
+  state = {
+    loaded: false,
+  }
 
-  componentDidMount() {
-    Font.loadAsync({
-      'Bungee': require('../assets/fonts/BungeeInline-Regular.ttf'),
-    });
+  componentWillMount() {
+    this._loadFontsAsync();
+  }
+   _loadFontsAsync = async () => {
+    await Font.loadAsync({'argana': require('../assets/fonts/Argana.ttf')});
+    this.setState({loaded: true});
   }
 
   render() {
+    if (!this.state.loaded) {
+      return <AppLoading />;
+    }
     return (
       <View style={styles.container}>
       <ImageBackground
           source={require('../assets/images/homeBackground.jpg')}
           style={styles.homeBackground}>
-      <View style={styles.headertext}>
-          <Text>PalmAlarm</Text>
-      </View>
+            <View style={styles.headertext}>
+              <Text style={styles.text}>PalmAlarm</Text>
+            </View>
+            <View>
+              <Button
+                style={styles.buttonStyle}
+                title="Scan"
+                onPress={ () => {
+                  this.props.navigation.dispatch(StackActions.reset({
+                  index: 0,
+                  actions: [
+                  NavigationActions.navigate({ routeName: 'InputScreen' })
+                  ],
+                  }))
+                }}
+              />
+            </View>
       </ImageBackground>
-     
-        <Button
-        title="Scan"
-        onPress={ () => {
-          this.props.navigation.dispatch(StackActions.reset({
-            index: 0,
-            actions: [
-              NavigationActions.navigate({ routeName: 'InputScreen' })
-            ],
-          }))
-        }}
-        />
       </View>
     );
   }
@@ -57,7 +67,19 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     marginTop: '35%',
-    fontFamily: 'Bungee'
+  },
+  text: {
+    fontFamily: 'argana',
+    fontSize: 35,
+    color: 'white'
+  },
+  buttonStyle: {
+    backgroundColor: "rgba(92, 99,216, 1)",
+    width: 300,
+    height: 45,
+    borderColor: "transparent",
+    borderWidth: 0,
+    borderRadius: 5
   }
 });
 
