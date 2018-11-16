ac273f1a7fc5944c7a7310e341a77fd5ef5bde91
AlexDresco <alexandra.d1510@gmail.com>
2018-11-16 10:10:43 +0000

    CSS

--- components/Results.js
+++ components/Results.js
@@ -51,17 +51,17 @@ export default class Results extends React.Component {
           if(this.state.dataSource.product.ingredients_from_palm_oil_n == 0){
           return(
           <View style={styles.container}>
-            <Text> {this.state.dataSource.product.product_name} </Text>
-            <Text> There are {this.state.dataSource.product.ingredients_from_palm_oil_n} ingredient from Palm oil in this </Text>
-            <Animatable.Text animation="zoomInUp"><Icon name="thumbs-up" size={150} color="#15d628"/></Animatable.Text>
+            <Text style={styles.text}> {this.state.dataSource.product.product_name} </Text>
+            <Text style={styles.text}> There are {this.state.dataSource.product.ingredients_from_palm_oil_n} ingredient from Palm oil in this </Text>
+            <Animatable.Text animation="zoomInUp"><Icon name="thumbs-up" size={150} color="#ffff"/></Animatable.Text>
           </View>
           );
           } else {
             return(
               <View style={styles.container}>
-                <Text> {this.state.dataSource.product.product_name} </Text>
-                <Text> There are {this.state.dataSource.product.ingredients_from_palm_oil_n} ingredient from Palm oil in this </Text>
-                <Animatable.Text animation="zoomInUp"><Icon name="thumbs-down" size={150} color="#ef2513"/></Animatable.Text>
+                <Text style={styles.text}> {this.state.dataSource.product.product_name} </Text>
+                <Text style={styles.text}> There are {this.state.dataSource.product.ingredients_from_palm_oil_n} ingredient from Palm oil in this </Text>
+                <Animatable.Text animation="zoomInUp"><Icon name="thumbs-down" size={150} color="#ffff"/></Animatable.Text>
               </View>
               );
           }
@@ -73,8 +73,12 @@ export default class Results extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#9fa5a1',
+    backgroundColor: '#829D96',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text:{
+    fontSize: 30,
+    fontFamily: 'Argana new'
+  }
 });
