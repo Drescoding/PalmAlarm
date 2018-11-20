888d35a88bbed90287b7d7cb22e8986c61ed8d80
AlexDresco <alexandra.d1510@gmail.com>
2018-11-20 13:09:53 +0000

    Refactored and tested with products in the comments

--- components/Results.js
+++ components/Results.js
@@ -31,66 +31,66 @@ export default class Results extends React.Component {
   }
 
   render() {
-
     if (this.state.isLoading) {
+      //Case 1: The data is not loaded from the API yet.
       return (
         <View style={styles.container}>
           <ActivityIndicator />
         </View>
       )
     } else {
-
       if (this.state.dataSource.status_verbose == "product not found") {
+        //Case 2: The barcode does not exist in the database
         return(
           <View style={styles.container}>
             <Text>Product not found</Text>
           </View>
         );
       } else {
+        //Case 3: The barcode is in the database
         const palm_ingredients_total = this.state.dataSource.product.ingredients_from_palm_oil_n;
         const palm_ingredients_list = this.state.dataSource.product.ingredients_from_palm_oil_tags;
         const maybe_total = this.state.dataSource.product.ingredients_that_may_be_from_palm_oil_n;
         const maybe_list = this.state.dataSource.product.ingredients_that_may_be_from_palm_oil_tags;
         const product_name = this.state.dataSource.product.product_name
 
-          if (palm_ingredients_total > 0 || (palm_ingredients_list != undefined && palm_ingredients_list.length > 0)) {
-          return(
-          <View style={styles.container}>
-            <Text style={styles.text}> {product_name} </Text>
-            <Text style={styles.text}> There are {palm_ingredients_total} ingredient from Palm oil in this </Text>
-            <Animatable.Text animation="zoomInUp"><Icon name="thumbs-down" size={150} color="#ffff"/></Animatable.Text>
-          </View>
-          );
-          } else if (maybe_total > 0 ||
-            //I think these aren't used because that's where the typo was.
-            ( maybe_list != undefined && maybe_list.length > 0 ) ) {
-
+          if (palm_ingredients_total > 0 || palm_ingredients_list.length > 0) {
+          //Case 3.1: There is definitely palm oil in there. Test with: 3700211234221
             return(
-              <View style={styles.container}>
-                <Text style={styles.text}> {product_name} </Text>
-                <Text style={styles.text}> These ingredients: {maybe_list} may involve the use of palm oil </Text>
-                <Animatable.Text animation="zoomInUp"><Icon name="thumbs-down" size={150} color="#ffff"/></Animatable.Text>
-              </View>
+            <View style={styles.container}>
+              <Text style={styles.text}> {product_name} </Text>
+              <Text style={styles.text}> There are {palm_ingredients_total} ingredient from Palm oil in this </Text>
+              <Animatable.Text animation="zoomInUp"><Icon name="thumbs-down" size={150} color="#ffff"/></Animatable.Text>
+            </View>);
+          } else if (maybe_total > 0 || maybe_list.length > 0){
+            //Case 3.2: There might be palm oil: palm_total and palm_list are 0 or empty, and maybe_list and maybe_total > 0.
+            //Test with: 3366321051563
+              return(
+                <View style={styles.container}>
+                  <Text style={styles.text}> {product_name} </Text>
+                  <Text style={styles.text}> These ingredients: {maybe_list} may involve the use of palm oil </Text>
+                </View>
               );
-          } else if (palm_ingredients_total === 0 && (palm_ingredients_list == undefined ||
-            palm_ingredients_list.length == 0) && maybe_total === 0
-            && (maybe_list == undefined || maybe_list.length == 0)
-            ) {
-          console.log("NOOO PALM OIL")
-          return(
-            <View style={styles.container}>
-              <Text style={styles.text}> {product_name} </Text>
-              <Text style={styles.text}> No palm oil!</Text>
-              <Animatable.Text animation="zoomInUp"><Icon name="thumbs-up" size={150} color="#ffff"/></Animatable.Text>
-            </View>
-            );
+          } else if (palm_ingredients_total === 0 && palm_ingredients_list.length == 0 && (maybe_total === 0 || maybe_total === undefined)
+            && (maybe_list == undefined || maybe_list.length == 0)){
+            //Case 3.3: There is absolutely no palm oil here: palm_total and palm_list = 0 and maybe_list and maybe_total = 0/empty.
+            //Test with: 737628064502
+              return(
+                <View style={styles.container}>
+                  <Text style={styles.text}> {product_name} </Text>
+                  <Text style={styles.text}> No palm oil!</Text>
+                  <Animatable.Text animation="zoomInUp"><Icon name="thumbs-up" size={150} color="#ffff"/></Animatable.Text>
+                </View>
+              );
           } else {
-          return(
-            <View style={styles.container}>
-              <Text style={styles.text}> {product_name} </Text>
-              <Text style={styles.text}> No information regarding presence of palm oil for this product</Text>
-            </View>
-            );
+            //Case 3.4: No information - palm_total = undefined, palm_list = undefined
+            //Test: 9557129019006
+              return(
+                <View style={styles.container}>
+                  <Text style={styles.text}> {product_name} </Text>
+                  <Text style={styles.text}> No information regarding presence of palm oil for this product</Text>
+                </View>
+              );
           }
         }
     }
